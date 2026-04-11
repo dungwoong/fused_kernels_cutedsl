4b91a128ab49f0ef23658e9fac953b223b55df71
Kevin Wang <imkevin.wang@mail.utoronto.ca>
2026-04-11 14:32:02 -0400

    add test gemm profiling

--- gemm/attempt1/separate_profile.py
+++ gemm/attempt1/separate_profile.py
@@ -1,16 +1,11 @@
 from gemm import *
+from profile_utils import run_experiment, get_args
 
 if __name__ == "__main__":
     print('Starting...')
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument("mode", type=str, choices=["debug", "speed", "ncu"])
-    args = parser.parse_args()
-    IS_NCU = args.mode == 'ncu'
-    IS_DEBUG = args.mode == 'debug'
-    IS_SPEED = args.mode == 'speed'
 
-    m, n, k = 65536, 4096, 8192
+    args = get_args()
+    m, n, k = args.m, args.n, args.k
     flops = 2 * m * n * k
 
     def get_tflops(time_ms):
@@ -23,12 +18,6 @@ if __name__ == "__main__":
     b = torch.randn((n, k), dtype=torch.bfloat16).to('cuda')
     c = torch.empty((m, n), dtype=torch.bfloat16).to('cuda')
     ref = a @ b.t()
-    convert_from_dlpack = lambda tensor: (
-        from_dlpack(tensor.detach(), assumed_align=16).mark_compact_shape_dynamic(
-            mode=0, stride_order=(0, 1)
-        )
-    )
-    # a_cute, b_cute, c_cute = [convert_from_dlpack(x) for x in (a, b, c)]
     current_stream = cuda.CUstream(torch.cuda.current_stream().cuda_stream)
     gemm = GemmSM90(tile_shape_mn=(128, 256), 
                     epi_tile_mn=(128, 32),
@@ -41,30 +30,32 @@ if __name__ == "__main__":
     b_c = make_fake_tensor(dtype, (n, k), div)
     c_c = make_fake_tensor(dtype, (m, n), divn)
     compiled_gemm = cute.compile(gemm, a_c, b_c, c_c, current_stream, options='--enable-tvm-ffi')
-    compiled_gemm(a, b, c, current_stream)
-    if not IS_NCU:
-        print('All close:', torch.allclose(ref, c))
-    if IS_DEBUG:
-        print(ref)
-        print(c)
-
-    if IS_DEBUG:
-        n_incorrect = c.numel() - ((c - ref).abs() < 0.001).sum()
-        print('n_incorrect :', n_incorrect)
-        print('n_nonzero :', (c != 0).sum())
-
+    tensors = (a, b)
     @torch.compile
-    def torch_gemm():
-        return a @ b.t()
-    
-    def cdsl_func(a, b):
-        o = torch.empty(a.shape[0], b.shape[0], dtype=torch.bfloat16, device='cuda')
-        compiled_gemm(a, b, o, current_stream)
-        return o
+    def torch_gemm(a_, b_):
+        return a_ @ b_.t()
 
-    if IS_SPEED:
-        my_ms = do_bench(lambda: cdsl_func(a, b))
-        other_ms = do_bench(torch_gemm)
-        print(f'{my_ms=}, {other_ms=}')
-        my_flops, other_flops = get_tflops(my_ms), get_tflops(other_ms)
-        print(f'{my_flops=}, {other_flops=}')
+    def cdsl_func(a_, b_):
+        o = torch.empty(a_.shape[0], b_.shape[0], dtype=torch.bfloat16, device='cuda')
+        compiled_gemm(a_, b_, o, current_stream)
+        return o
+    
+    run_experiment(args, tensors, torch_gemm, tensors, cdsl_func)
+    # compiled_gemm(a, b, c, current_stream)
+    # if not IS_NCU:
+    #     print('All close:', torch.allclose(ref, c))
+    # if IS_DEBUG:
+    #     print(ref)
+    #     print(c)
+
+    # if IS_DEBUG:
+    #     n_incorrect = c.numel() - ((c - ref).abs() < 0.001).sum()
+    #     print('n_incorrect :', n_incorrect)
+    #     print('n_nonzero :', (c != 0).sum())
+
+    # if IS_SPEED:
+    #     my_ms = do_bench(lambda: cdsl_func(a, b))
+    #     other_ms = do_bench(torch_gemm)
+    #     print(f'{my_ms=}, {other_ms=}')
+    #     my_flops, other_flops = get_tflops(my_ms), get_tflops(other_ms)
+    #     print(f'{my_flops=}, {other_flops=}')
