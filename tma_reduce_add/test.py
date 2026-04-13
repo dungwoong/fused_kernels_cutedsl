00a7098ee0034108ccf1702471e896b809b2be25
Kevin Wang <wangke61@cs.toronto.edu>
2026-04-13 16:32:34 -0400

    fix tma reduce add example

--- tma_reduce_add/test.py
+++ tma_reduce_add/test.py
@@ -1,4 +1,5 @@
 import torch
+import cuda.bindings.driver as cuda
 import cutlass
 from cutlass import cute
 import cutlass.utils.hopper_helpers as sm90_utils
@@ -7,8 +8,20 @@ from cdsl_helpers import shared
 import my_utils
 from cdsl_fn_utils import compile_cutedsl
 
-DTYPE = cutlass.BFloat16
+DTYPE = cutlass.Float32
+DTYPE_TORCH = torch.float32
 BUF_ALIGN = 1024
+
+@cute.jit
+def print0(x):
+    tidx, _, _ = cute.arch.thread_idx()
+    bidx, bidy, bidz = cute.arch.block_idx()
+    if cutlass.const_expr(isinstance(x, cute.TensorSSA)):
+        if tidx == 0 and bidx == 0 and bidy == 0 and bidz == 0:
+            cute.print_tensor(x)
+    else:
+        if tidx == 0 and bidx == 0 and bidy == 0 and bidz == 0:
+            cute.printf(x)
 
 class Kernel:
     def __init__(self, tile_m, tile_n):
@@ -21,18 +34,20 @@ class Kernel:
     def __call__(
         self,
         mA: cute.Tensor,
-        mO: cute.Tensor):
+        mO: cute.Tensor,
+        stream: cuda.CUstream):
         self._set_smem_layouts()
         self._set_shared_storage()
         self.n_bytes = cute.size_in_bytes(DTYPE, self.smem_layout)
         atom_ld, tensor_ld, atom_st, tensor_st = self._get_tma_copy_attrs(mA, mO)
         self.kernel(
             atom_ld, tensor_ld, atom_st, tensor_st, self.smem_layout
-        ).launch(grid=[132], block=[128, 1, 1])
+        ).launch(grid=[1024], block=[128, 1, 1])
     
     @cute.kernel
     def kernel(self, atom_ld, tensor_ld, atom_st, tensor_st, smem_layout):
         warp_idx = cute.arch.make_warp_uniform(cute.arch.warp_idx())
+        tidx, _, _ = cute.arch.thread_idx()
 
         smem = cutlass.utils.SmemAllocator()
         storage = smem.allocate(self.smem_cls)
@@ -52,14 +67,17 @@ class Kernel:
         prod_state = pipeline.make_pipeline_state(pipeline.PipelineUserType.Producer, 1)
         cons_state = pipeline.make_pipeline_state(pipeline.PipelineUserType.Consumer, 1)
         if warp_idx == 0:
+            pipe.producer_acquire(prod_state)
             shared.tma_copy(atom_ld, tensor_ld, sO, self.tile_m, self.tile_n, 0, 0, pipe, prod_state)
         pipe.consumer_wait(cons_state)
+
+        cute.arch.sync_threads()
 
         # honestly everything is happening in the async proxy so I think I can just go for the store
         store_O, _, _ = my_utils.tma_get_copy_fn(
             atom_st, 0, cute.make_layout(1), sO, tensor_st, single_stage=True
         )
-        if warp_idx == 4:
+        if warp_idx == 0:
             store_O()
             cute.arch.cp_async_bulk_commit_group()
             cute.arch.cp_async_bulk_wait_group(0)
@@ -83,19 +101,19 @@ class Kernel:
 
     def _get_tma_copy_attrs(self, mA: cute.Tensor, mO: cute.Tensor):
         atom_load = cute.nvgpu.cpasync.CopyBulkTensorTileG2SOp()
-        atom_store = cute.nvgpu.cpasync.CopyReduceBulkTensorTileS2GOp()
+        atom_store = cute.nvgpu.cpasync.CopyReduceBulkTensorTileS2GOp(cute.nvgpu.cpasync.ReductionOp.ADD)
 
         tma_atom_load, tma_tensor_a = cute.nvgpu.cpasync.make_tiled_tma_atom(
             atom_load,
             mA,
-            self.smem_layout
+            cute.select(self.smem_layout, mode=[0, 1]),
             (self.tile_m, self.tile_n)
         )
 
         tma_atom_store, tma_tensor_o = cute.nvgpu.cpasync.make_tiled_tma_atom(
             atom_store,
             mO,
-            self.smem_layout,
+            cute.select(self.smem_layout, mode=[0, 1]),
             (self.tile_m, self.tile_n)
         )
 
@@ -103,8 +121,12 @@ class Kernel:
 
 if __name__ == '__main__':
     k = Kernel(128, 128)
-    a = torch.ones((128, 128), dtype=torch.bfloat16, device='cuda')
+    a = torch.ones((128, 128), dtype=DTYPE_TORCH, device='cuda')
+    a[:, 1] = a[:, 1] * 2 # check indexing is right by setting a column differently
     o = torch.zeros_like(a)
+    # print(o)
 
     compiled_kernel = compile_cutedsl((a, o), k)
-    compiled_kernel(a, o)
+    compiled_kernel(a, o, None)
+    compiled_kernel(a, o, None) # should give us 2048
+    print(o)
